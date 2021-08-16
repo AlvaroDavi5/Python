a6d30d2240ea575b4e5a66d379dc5c06c2ae8ce8
Álvaro Davi <alvaro-davi1@hotmail.com>
2021-08-16 01:02:56 -0300

    Updated NoSQL sample

--- manipulacao de dados/MongoDB - non-relational document-oriented database/main.py
+++ manipulacao de dados/MongoDB - non-relational document-oriented database/main.py
@@ -1,13 +1,13 @@
 import datetime as date
 from pymongo import MongoClient
 
-conn = MongoClient('localhost', 27017) # make connection with database
+conn = MongoClient('localhost', 27017) # make connection with database (host and port)
 
 # just one instance of MongoDB can support multiple databases
 db = conn.cadastrodb # database cadastrodb
 
  # a collection its a group of documents stored in MongoDB
-collection = db.cadastrodb # like tables in RDBMS
+collection = db.cadastrodb # collections in DOC-DBMS (NoSQL) are like tables in RDBMS (SQL)
 
  # dicts
 post1 = {
@@ -17,14 +17,14 @@ post1 = {
 	'data_cadastro':date.datetime.utcnow()
 }
 post2 = {
-	'codigo':"ID-357689",
+	'codigo':"ID-357689", # fields in DOC-DBMS (NoSQL) are like columns in RDBMS (SQL)
 	'prod_nome':"Televisor",
 	'marcas':["lg", "panasonic", "sansung"],
 	'data_cadastro':date.datetime.utcnow()
 }
 
 collection = db.posts
-post_id = collection.insert_one(post1)
+post_id = collection.insert_one(post1) # documents in DOC-DBMS (NoSQL) are like lines in RDBMS (SQL)
 print(post_id.inserted_id)
 post_id = collection.insert_one(post2).inserted_id
 print(post_id)
